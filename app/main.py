f5270028babf473b161cca896a3a45e91dc53bf5
Ahmed Azaan <azaan@outlook.com>
2014-09-13 23:32:12 +0530

    remove debug template handling

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-import os
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
-if os.environ.get('PROD', None) is None:
-    app.debug = True
-    print('DEBUG')
 
 @app.route('/')
 def index():
-    if app.debug == True:
-        template = 'dist/home.html'
-    else:
-        template = 'home.html'
-
-    return render_template(template)
+    return render_template('dist/home.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)

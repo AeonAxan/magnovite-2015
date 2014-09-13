44fee4d921ff48498c873de1bfb94bf9d7ab3ef1
Ahmed Azaan <azaan@outlook.com>
2014-09-13 21:32:51 +0530

    setup gulp build system

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    if app.debug:
+        template = 'home.html'
+    else:
+        template = 'dist/home.html'
+
+    return render_template(template)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,20 @@ var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
+var clean = require('gulp-clean');
+
+gulp.task('dist', ['usemin'], function() {
+    'use strict';
+
+    // move css files
+    var files = [
+        'app/templates/dist/static/dist/*.css',
+        'app/templates/dist/static/dist/*.js'
+    ];
+
+    gulp.src(files, {base: 'app/templates/dist/static/'})
+        .pipe(gulp.dest('app/static/'));
+});
 
 gulp.task('usemin', function() {
     'use strict';
@@ -13,8 +27,19 @@ gulp.task('usemin', function() {
     gulp.src('app/templates/*.html')
         .pipe(usemin({
             css: [minifyCss(), 'concat'],
-            html: [minifyHtml({empty: true, loose:true})],
-            js: [uglify(), rev()]
+            js: [uglify(), 'concat']
         }))
-        .pipe(gulp.dest('app/templates/dist/'));
+        .pipe(gulp.dest('app/templates/dist'));
+});
+
+gulp.task('clean', function() {
+    'use strict';
+
+    var dirs = [
+        'app/templates/dist',
+        'app/static/dist'
+    ];
+
+    gulp.src(dirs, {read: false})
+        .pipe(clean());
 });

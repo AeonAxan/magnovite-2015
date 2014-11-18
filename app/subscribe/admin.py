d68017d1e60c4cf6e85c912ef776dd7d134dfabc
Ahmed Azaan <azaan@outlook.com>
2014-11-18 17:18:26 +0530

    add subscription admin

--- app/subscribe/admin.py
+++ app/subscribe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.subscribe.models import Subscription
+
+@admin.register(Subscription)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('email',)

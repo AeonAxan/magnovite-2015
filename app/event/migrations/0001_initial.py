18bfeb621928ccc91587d5674185de37e803608f
Ahmed Azaan <azaan@outlook.com>
2014-12-01 02:23:36 +0530

    add admin template, base migrations

--- app/event/migrations/0001_initial.py
+++ app/event/migrations/0001_initial.py
@@ -2,28 +2,43 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import multiselectfield.db.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('main', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Event',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=100)),
-                ('slug', models.SlugField()),
-                ('quote', models.CharField(max_length=50)),
-                ('info', models.TextField(help_text='Please write in Markdown')),
-                ('cash_prize', models.IntegerField()),
-                ('time', models.CharField(max_length=30)),
-                ('venue', models.CharField(max_length=50)),
-                ('technical', models.BooleanField(default=True)),
-                ('tags', models.CharField(blank=True, help_text='Comma seperated list of tags', max_length=100, null=True)),
-                ('cover_picture', models.ImageField(blank=True, upload_to='covers/', null=True)),
+                ('slug', models.SlugField(help_text='The event url, use all simple and - as a seperator, Eg: junkyard-wars')),
+                ('quote', models.CharField(max_length=50, help_text='Text displayed on the cards in /events/')),
+                ('info', models.TextField(help_text='Please write in Markdown (Use headings, bold, italic, lists)')),
+                ('cash_prize', models.IntegerField(help_text='Numeric, Eg: 5000')),
+                ('date', models.IntegerField(max_length=2, help_text='Eg: 21')),
+                ('time', models.CharField(max_length=30, help_text='(Start time), Eg: 2pm')),
+                ('venue', models.CharField(max_length=50, help_text='Eg: Room 243, Block 2')),
+                ('technical', models.BooleanField(default=True, help_text='If cultural set to false')),
+                ('tags', multiselectfield.db.fields.MultiSelectField(choices=[('CSE', 'Computer Science'), ('EC', 'Electronics'), ('MECH', 'Mechanical'), ('CIVIL', 'Civil')], max_length=17, null=True, blank=True)),
+                ('cover_picture', models.ImageField(upload_to='covers/', null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Registration',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('team_id', models.IntegerField(null=True, blank=True)),
+                ('event', models.ForeignKey(to='event.Event')),
+                ('profile', models.ForeignKey(to='main.Profile')),
             ],
             options={
             },
--- app/main/migrations/0001_initial.py
+++ app/main/migrations/0001_initial.py
@@ -3,27 +3,47 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('event', '__first__'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='MUser',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
-                ('email', models.EmailField(max_length=255, verbose_name='Email Address', unique=True)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('email', models.EmailField(max_length=255, unique=True, verbose_name='Email Address')),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_admin', models.BooleanField(default=False)),
                 ('is_staff', models.BooleanField(default=False)),
             ],
             options={
                 'verbose_name': 'Magnovite User',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('auth_provider', models.CharField(blank=True, max_length=30)),
+                ('active_email', models.EmailField(max_length=75)),
+                ('name', models.CharField(blank=True, max_length=50)),
+                ('mobile', models.CharField(help_text='Without +91', blank=True, max_length=10)),
+                ('college', models.CharField(blank=True, max_length=50)),
+                ('city', models.CharField(blank=True, max_length=50)),
+                ('year', models.IntegerField(help_text='Studying in year (1, 2, 3, 4, 5)?', blank=True, max_length=1, null=True)),
+                ('registered_events', models.ManyToManyField(to='event.Event', through='event.Registration')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
             },
             bases=(models.Model,),
         ),

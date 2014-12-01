e4952647ea77e77a0eba1325134fefbf73e75ff3
Ahmed Azaan <azaan@outlook.com>
2014-12-01 21:44:51 +0530

    make event page work from db

--- app/event/views.py
+++ app/event/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
 
+from .models import Event
+
 
 @staff_member_required
 def index(req):
@@ -10,7 +12,12 @@ def index(req):
     else:
         template = 'magnovite/dist/events.html'
 
-    return render(req, template)
+    events = Event.objects.all()
+
+    return render(req, template, {
+        'events': events
+    })
+
 
 def details(req):
  	return render(req, 'magnovite/eventDetails.html')

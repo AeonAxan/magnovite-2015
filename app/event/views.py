5011cedbbac9bdacb3c1e74173ffa4554db9df6c
Ahmed Azaan <azaan@outlook.com>
2014-12-01 21:50:34 +0530

    Revert "make event page work from db"
    This reverts commit e4952647ea77e77a0eba1325134fefbf73e75ff3.

--- app/event/views.py
+++ app/event/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
 
-from .models import Event
-
 
 @staff_member_required
 def index(req):
@@ -12,12 +10,7 @@ def index(req):
     else:
         template = 'magnovite/dist/events.html'
 
-    events = Event.objects.all()
-
-    return render(req, template, {
-        'events': events
-    })
-
+    return render(req, template)
 
 def details(req):
  	return render(req, 'magnovite/eventDetails.html')

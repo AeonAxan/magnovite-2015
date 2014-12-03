9bd7606551991465bfa9710cd072b11596760982
Ahmed Azaan <azaan@outlook.com>
2014-12-03 14:17:51 +0530

    have cover picture, event head pictures as static files

--- app/event/models.py
+++ app/event/models.py
@@ -46,25 +46,30 @@ class Event(models.Model):
         help_text='Maximum number of people in a team (If individual: 1)',
         default=1
     )
-
     # if not technical, then cultural
     technical = models.BooleanField(default=True, help_text='If cultural set to false')
 
     # This is a comma seperated field
     tags = MultiSelectField(choices=TECHNICAL_TAGS, blank=True, null=True)
 
-    cover_picture = models.ImageField(
-        upload_to='covers/',
-        blank=True,
-        null=True
+    # event heads
+    picture_one = models.BooleanField(
+        help_text='Does event head 1 have a picture? (name: img/events/[slug]_h1.jpg)',
+        default=False
     )
+
+    picture_two = models.BooleanField(
+        help_text='Does event head 2 have a picture? (name: img/events/[slug]_h2.jpg)',
+        default=False
+    )
+
 
     def is_complete(self):
         """
         Are all the details of this event complete,
         we only need to verify optional fields
         """
-        return bool(self.date and self.time and self.venue and self.cover_picture)
+        return bool(self.date and self.time and self.venue)
     is_complete.boolean = True
 
     def is_team(self):
@@ -101,6 +106,21 @@ class Event(models.Model):
                 return row[1]
 
         return ""
+
+    def cover_picture_path(self):
+        return 'img/events/' + self.slug + '_cover.jpg'
+
+    def event_head_p1(self):
+        if self.picture_one:
+            return 'img/events/' + self.slug + '_h1.jpg'
+        else:
+            return 'img/events/head_default.jpg'
+
+    def event_head_p1(self):
+        if self.picture_one:
+            return 'img/events/' + self.slug + '_h2.jpg'
+        else:
+            return 'img/events/head_default.jpg'
 
     def get_absolute_url(self):
         return reverse('event_details', kwargs={'slug': self.slug})

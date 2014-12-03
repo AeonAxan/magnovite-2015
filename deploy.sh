1ff6cf9c60fc515d21ab0fa96fcce8a26cbab351
Ahmed Azaan <azaan@outlook.com>
2014-12-03 10:19:21 +0530

    staging: fix events page

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,30 @@
 #! /bin/env bash
 
-echo $1
-gulp dist
-git add .
-git commit -m "$1"
-git push heroku master
+if [ "$#" -ne 2 ]; then
+    echo "Usage: <production|staging> <commit message> [--no-dist]"
+    exit 1
+fi
+
+echo ">>>> Env: $1"
+echo ">>>> Message: $2"
+
+if echo $* | grep -e "--no-dist" -q
+then
+  echo ">>>> Skipping Dist step"
+else
+  gulp dist
+  git add .
+  git commit -m "$1: $2"
+fi
+
+if [ "$1" -eq "production" ]
+then
+    git push heroku production:master
+    APP=magnovite
+else
+    git push staging staging:master
+    APP=magnovite-staging
+fi
+
+echo ">>>> Migrationg heroku app $APP"
+heroku run ./manage.py migrate --app="$APP"

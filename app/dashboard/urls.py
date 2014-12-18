7cd58adf3b41818278ea0d15030f96fc6c085174
Ahmed Azaan <azaan@outlook.com>
2014-12-18 12:04:58 +0530

    api for analytics

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('app.dashboard.views',
-    url('^$', 'index', name='dashboard'),
-    url('^capture/', 'capture', name='dashboard:capture'),
+    url(r'^$', 'index', name='dashboard'),
+    url(r'^capture/', 'capture', name='dashboard:capture'),
+    url(r'^data/(?P<id>\d+)/', 'data', name='dashboard:data'),
 )
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,13 +1,16 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
-from django.conf import settings
-from django.http import Http404, HttpResponse
+import json
 
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.http import Http404, JsonResponse, HttpResponse
+
+from app.event.models import Event
 from .models import Analytics
 
 
 def index(req):
-    if not req.user.is_staff:
+    if not (req.user.is_staff and req.user.has_perm('event.change_event')):
         raise Http404
 
     if settings.DEBUG:
@@ -15,12 +18,41 @@ def index(req):
     else:
         template = 'magnovite/dist/dashboard.html'
 
-    return render(req, template)
+    qs = Event.objects.all()
+    if not req.user.is_superuser and req.user.has_perm('event.change_own'):
+        qs = qs.filter(heads=req.user.profile)
+
+    return render(req, template, {
+        'events': qs
+    })
 
 
-def data(req):
-    if not req.user.is_staff:
+def data(req, id):
+    if not (req.user.is_staff and req.user.has_perm('event.change_event')):
         raise Http404
+
+    id = int(id)
+    event = get_object_or_404(Event, id=id)
+
+    if req.user.has_perm('event.change_own'):
+        if not event.heads.filter(id=req.user.profile.id).exists():
+            return JsonResponse({
+                'error': 'permission_denied',
+            }, status=400)
+
+    out = []
+    for day in Analytics.objects.all():
+        event = next(filter(lambda o: o['id'] == id, json.loads(day.data)))
+        out.append({
+            'id': id,
+            'title': event['title'],
+            'date': day.date.strftime('%d/%m'),
+            'views': event['views'],
+            'registrations': event['registrations']
+        })
+
+    return JsonResponse(out, safe=False)
+
 
 @csrf_exempt
 def capture(req):

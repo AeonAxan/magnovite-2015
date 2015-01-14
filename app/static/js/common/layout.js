df8b878561ecc1538ec7ccbeb79242e0927e7a46
deepak <deepakbenny@outlook.com>
2015-01-14 16:52:53 +0530

    full scroll events page

--- app/static/js/common/layout.js
+++ app/static/js/common/layout.js
@@ -1,11 +1,17 @@
+var app = app || {};
+
 (function() {
     'use strict';
 
+
+    if(app.mobile){
+        return
+    }
     /**
      * If body has class browser-height set the body's
      * height to the browser height, and adjust on resize
      */
-    if ($(document.body).hasClass('browser-height')) {
+    if ($(document.body).hasClass('browser-height') && app.desktop) {
         $(document.body).height(window.innerHeight);
 
         var bannerHeight = $('.banner').height() || 0;

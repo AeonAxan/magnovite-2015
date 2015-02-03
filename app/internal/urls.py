ca07b0fc7af7722f61d30350e3e51a53e99bdf3a
Ahmed Azaan <azaan@outlook.com>
2015-02-03 22:51:23 +0530

    use api url

--- app/internal/urls.py
+++ app/internal/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('app.internal.views',
     url(r'^register/$', 'register_view', name='register'),
-    url(r'^register/create/$', 'register_create', name='register:create'),
+    url(r'^api/register/$', 'register_create', name='register:create'),
 )

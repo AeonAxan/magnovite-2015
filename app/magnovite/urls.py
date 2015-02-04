be80b0c2106a9e6e07870d5e62c40580d2574b03
Ahmed Azaan <azaan@outlook.com>
2015-02-04 11:49:12 +0530

    production: dist: new changes

--- app/magnovite/urls.py
+++ app/magnovite/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^quest/', include('app.quest.urls')),
     url(r'^dashboard/', include('app.dashboard.urls')),
     url(r'^payment/', include('app.payment.urls')),
-    url(r'^workshop/', include('app.workshop.urls')),
+    url(r'^workshops/', include('app.workshop.urls')),
     url(r'^internal/', include('app.internal.urls')),
 
     url(r'^admin/', include(admin.site.urls)),

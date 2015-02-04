98e52ff12fd3e82b2d43f2782e9f515ad5f2ea8c
Ahmed Azaan <azaan@outlook.com>
2015-02-04 14:39:49 +0530

    use workshop model to populate page

--- app/workshop/models.py
+++ app/workshop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=50)
+    desc_1 = models.TextField()
+    desc_2 = models.TextField()
+
+    std_1_name = models.CharField(max_length=50)
+    std_1_mobile = models.CharField(max_length=10)
+    std_2_name = models.CharField(max_length=50)
+    std_2_mobile = models.CharField(max_length=10)
+
+    faculty_name = models.CharField(max_length=50)
+    faculty_mobile = models.CharField(max_length=10)
+
+    price = models.IntegerField(max_length=5)
+
+    img_big = models.URLField(help_text='400x400')
+    img_small = models.URLField(help_text='120x120')
--- app/workshop/views.py
+++ app/workshop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.conf import settings
 
+from .models import Workshop
+
+
 def index(req):
     if settings.DEBUG:
         template = 'magnovite/workshop.html'
     else:
         template = 'magnovite/dist/workshop.html'
 
-    return render(req, template)
+    return render(req, template, {
+        'workshops': Workshop.objects.all()
+    })

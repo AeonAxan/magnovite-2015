6883e189b8aa5395247216f8ae2406bea0911754
Ahmed Azaan <azaan@outlook.com>
2015-02-05 22:01:17 +0530

    remove pytz from the migration

--- app/event/migrations/0024_registration_created.py
+++ app/event/migrations/0024_registration_created.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import datetime
-import pytz
 
 class Migration(migrations.Migration):
 
@@ -15,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='registration',
             name='created',
-            field=models.DateTimeField(default=datetime.datetime(2015, 2, 5, 21, 42, 39, 842064, pytz.UTC), auto_now_add=True),
+            field=models.DateTimeField(default=datetime.datetime(2015, 2, 5, 21, 42, 39, 842064), auto_now_add=True),
             preserve_default=False,
         ),
     ]

0f4faba6fb1945e54c9bcf6b777ee4d3fdb4dc21
Ahmed Azaan <azaan@outlook.com>
2015-02-12 18:25:37 +0530

    fix teamid in url

--- app/event/urls.py
+++ app/event/urls.py
@@ -4,10 +4,10 @@ urlpatterns = patterns('app.event.views',
     url('^$', 'index', name='events'),
 
     url('^api/register/(?P<id>\d+)/$', 'register', name='register'),
-    url('^api/register/(?P<id>\d+)/(?P<team_id>[a-f0-9]{5})/$', 'register', name='register'),
+    url('^api/register/(?P<id>\d+)/(?P<team_id>[tg]-[a-f0-9]{5})/$', 'register', name='register'),
 
     url('^api/unregister/(?P<id>\d+)/$', 'unregister', name='unregister'),
-    url('^api/unregister/(?P<id>\d+)/(?P<team_id>[a-f0-9]{5})/$', 'unregister', name='unregister'),
+    url('^api/unregister/(?P<id>\d+)/(?P<team_id>[tg]-[a-f0-9]{5})/$', 'unregister', name='unregister'),
 
     url('^(?P<slug>[-\w]+)/', 'details', name='event_details')
 )

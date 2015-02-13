3def7b9c710b88ea60888f01ef17b7603195ea9c
Ahmed Azaan <azaan@outlook.com>
2015-02-14 00:06:37 +0530

    show messages in thread

--- app/message/admin.py
+++ app/message/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 
 from .models import Thread, Message
 
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 1
+
 @admin.register(Thread)
 class ThreadAdmin(admin.ModelAdmin):
     list_display = ['id', 'profile', 'is_pending']
     list_filter = ['is_pending']
-
+    inlines = [MessageInline]
 
 @admin.register(Message)
 class MessaegAdmin(admin.ModelAdmin):
